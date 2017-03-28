1d84637f1bce31ee5c427b75158239327e0b746f
Vlad Feninets <v.feninets@gmail.com>
2017-03-29 01:25:23 +0300

    prettify after release course

--- setup-env/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import TestComponent from './TestComponent'
-
-render(
-	<TestComponent />,
-	document.querySelector('#mount_place')
-)
